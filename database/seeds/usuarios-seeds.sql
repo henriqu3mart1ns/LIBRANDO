0fa37c98082b555f8ea27afd8e27a9ad7886b470
henrique <160766948+henriqu3mart1ns@users.noreply.github.com>
2026-09-01 21:47:29 -0300

    Update usuarios-seeds.sql to exclude nome field
    Removed 'nome' field from usuarios insert statement.

--- database/seeds/usuarios-seeds.sql
+++ database/seeds/usuarios-seeds.sql
@@ -1,8 +1,7 @@
 USE librando;
 
-INSERT INTO usuarios (nome, email, senha)
+INSERT INTO usuarios (email, senha)
 VALUES (
-    'Usuario Teste',
     'teste@email.com',
     '$2y$10$hjziW7/q1IWhiBPdnwb26uor384NLZISafl.Vz2KLd6BTB27uNsCW'
 );
